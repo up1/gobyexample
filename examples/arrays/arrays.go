2918f0fd7b8a63a7fc94fa2caf76c0e9c1cab462
armeo <armchockaumnuai@gmail.com>
2016-02-26 12:27:16 +0700

    แปลเรื่อง Arrays

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -1,5 +1,4 @@
-// In Go, an _array_ is a numbered sequence of elements of a
-// specific length.
+// ในภาษาโก _อาเรย์_ คือตัวเลขลำดับของสมาชิกที่มีความยาวคงที่
 
 package main
 
@@ -7,31 +6,26 @@ import "fmt"
 
 func main() {
 
-    // Here we create an array `a` that will hold exactly
-    // 5 `int`s. The type of elements and length are both
-    // part of the array's type. By default an array is
-    // zero-valued, which for `int`s means `0`s.
+    // สร้างอาเรย์ `a` ชนิด int ขนาด 5 element
+    // โดยที่ค่าเริ่มต้นในแต่ละ element คือ `0`
     var a [5]int
     fmt.Println("emp:", a)
 
-    // We can set a value at an index using the
-    // `array[index] = value` syntax, and get a value with
-    // `array[index]`.
+    // เราสามารถ set และ get ค่าจากอาเรย์ได้โดยใช้ index
+    // set โดยใช้รูปแบบ `array[index] = value`,
+    // และ get ค่าโดยใช้รูปแบบ `array[index]`.
     a[4] = 100
     fmt.Println("set:", a)
     fmt.Println("get:", a[4])
 
-    // The builtin `len` returns the length of an array.
+    // ใช้ฟังชั่น `len` เมื่อเราอยากรู้ขนาดของอาเรย์.
     fmt.Println("len:", len(a))
 
-    // Use this syntax to declare and initialize an array
-    // in one line.
+    // สำหรับการประกาศและกำหนดค่าเริ่มต้นให้กับอาเรย์ในหนึ่งบรรทัดจะใช้รูปแบบ.
     b := [5]int{1, 2, 3, 4, 5}
     fmt.Println("dcl:", b)
 
-    // Array types are one-dimensional, but you can
-    // compose types to build multi-dimensional data
-    // structures.
+    // ตัวแปรชนิดอาเรย์จะเป็นแบบหนึ่งมิติ แต่เราก็สามารถประกอบให้เป็นหลายๆ มิติได้เช่นกัน
     var twoD [2][3]int
     for i := 0; i < 2; i++ {
         for j := 0; j < 3; j++ {
--- examples/arrays/arrays.sh
+++ examples/arrays/arrays.sh
@@ -1,5 +1,5 @@
-# Note that arrays appear in the form `[v1 v2 v3 ...]`
-# when printed with `fmt.Println`.
+# ที่ console output จะพิมพ์ออกมาเป็น `[v1 v2 v3 ...]`
+# เมื่อเราพิมพ์ด้วย `fmt.Println`.
 $ go run arrays.go
 emp: [0 0 0 0 0]
 set: [0 0 0 0 100]
@@ -8,5 +8,5 @@ len: 5
 dcl: [1 2 3 4 5]
 2d:  [[0 1 2] [1 2 3]]
 
-# You'll see _slices_ much more often than arrays in
-# typical Go. We'll look at slices next.
+# ซึ่งโดยปกติในภาษาโก คุณจะเห็น slices มากกว่าการใช้อาเรย์
+# ดังนั้นในหัวข้อถัดไปเรามาดูเรื่อง slices กัน.
